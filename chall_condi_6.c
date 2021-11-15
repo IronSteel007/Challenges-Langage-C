67ce3a2e199e3c6b2a0710b5cc1bf491629396a3
IronSteel007 <yahya.kouzbour.tech@gmail.com>
2021-11-15 06:38:23 +0100

    work hard no sleep

--- chall_condi_6.c
+++ chall_condi_6.c
@@ -19,62 +19,62 @@ int main()
 	printf("5. Secondes \n");
 	scanf("%ld", &choix);
 
+
 	if (Annee <= 0 ) {
 		printf("Impossible");
 	}
 
-
-  else if (Annee % 4 == 0 ) {	
-  
-		  switch (choix) {
-				case 1 :
-					Annee = 12;
-					printf("%ld Mois ", Annee) ;
-					break;
-				case 2 : 
-					Annee = 12 * 30 + 6;
-					printf("%ld Jours ",Annee ) ;
-					break;
-				case 3 : 
-					Annee = 24*(12 * 30 + 6) ;
-					printf("%ld Heures ", Annee) ;
-					break;
-				case 4 :
-					Annee = 60*(24*(12 * 30 + 6)) ;
-					printf("%ld Minutes ", Annee) ;
-					break;
-				case 5 :
-					Annee = 60*(60*(24*(12 * 30 + 6))) ;
-					printf("%ld Secondes ", Annee);
-					break;
-			}
-
-
-}
+	  else if (Annee % 4 == 0 ) {	
+	  
+			  switch (choix) {
+					case 1 :
+						Annee = 12;
+						printf("%ld Mois ", Annee) ;
+						break;
+					case 2 : 
+						Annee = 12 * 30 + 6;
+						printf("%ld Jours ",Annee ) ;
+						break;
+					case 3 : 
+						Annee = 24*(12 * 30 + 6) ;
+						printf("%ld Heures ", Annee) ;
+						break;
+					case 4 :
+						Annee = 60*(24*(12 * 30 + 6)) ;
+						printf("%ld Minutes ", Annee) ;
+						break;
+					case 5 :
+						Annee = 60*(60*(24*(12 * 30 + 6))) ;
+						printf("%ld Secondes ", Annee);
+						break;
+				}
 	
-	 
-  else
-		    switch (choix) {
-						case 1 :
-					Annee = 12;
-					printf("%ld Mois ", Annee) ;
-					break;
-				case 2 : 
-					Annee = 12 * 30 + 5;
-					printf("%ld Jours ",Annee ) ;
-					break;
-				case 3 : 
-					Annee = 24*(12 * 30 + 5) ;
-					printf("%ld Heures ", Annee) ;
-					break;
-				case 4 :
-					Annee = 60*(24*(12 * 30 + 5)) ;
-					printf("%ld Minutes ", Annee) ;
-					break;
-				case 5 :
-					Annee = 60*(60*(24*(12 * 30 + 5))) ;
-					printf("%ld Secondes ", Annee);
-					break;
-					}
+	
+	}
+		
+		 
+	  else
+			    switch (choix) {
+							case 1 :
+						Annee = 12;
+						printf("%ld Mois ", Annee) ;
+						break;
+					case 2 : 
+						Annee = 12 * 30 + 5;
+						printf("%ld Jours ",Annee ) ;
+						break;
+					case 3 : 
+						Annee = 24*(12 * 30 + 5) ;
+						printf("%ld Heures ", Annee) ;
+						break;
+					case 4 :
+						Annee = 60*(24*(12 * 30 + 5)) ;
+						printf("%ld Minutes ", Annee) ;
+						break;
+					case 5 :
+						Annee = 60*(60*(24*(12 * 30 + 5))) ;
+						printf("%ld Secondes ", Annee);
+						break;
+						}
    return 0; 
 }
